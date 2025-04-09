8626ef6da500eccb970e3f8437f17ae7cb105ce1
ice breaker <1324318532@qq.com>
2025-04-09 11:45:56 +0800

    chore: commit code

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,31 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
 import tailwindcss from '@tailwindcss/postcss'
-export default defineConfig(async () => {
-    return {
-        plugins: [
-            uni(),
-            UnifiedViteWeappTailwindcssPlugin(
-                {
-                    rem2rpx: true,
-                    tailwindcssPatcherOptions: {
-                        patch: {
-                            tailwindcss: {
-                                v4: {
-                                    base: __dirname
-                                }
+export default defineConfig({
+    plugins: [
+        uni(),
+        UnifiedViteWeappTailwindcssPlugin(
+            {
+                rem2rpx: true,
+                tailwindcssPatcherOptions: {
+                    patch: {
+                        tailwindcss: {
+                            v4: {
+                                base: __dirname
                             }
                         }
                     }
                 }
-            )
-        ],
-        css: {
-            postcss: {
-                plugins: [
-                    tailwindcss()
-                ]
             }
+        )
+    ],
+    css: {
+        postcss: {
+            plugins: [
+                tailwindcss({
+                    base: __dirname
+                })
+            ]
         }
     }
 });
