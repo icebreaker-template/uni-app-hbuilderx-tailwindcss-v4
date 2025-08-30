83034adff2dca8be22fad8b2b7f59fdd5bd70d18
ice breaker <1324318532@qq.com>
2025-08-31 01:40:00 +0800

    chore(deps): upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,17 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
 import tailwindcss from '@tailwindcss/postcss'
+import path from 'path'
 export default defineConfig({
     plugins: [
         uni(),
         UnifiedViteWeappTailwindcssPlugin(
             {
                 rem2rpx: true,
-                tailwindcssPatcherOptions: {
-                    patch: {
-                        tailwindcss: {
-                            v4: {
-                                base: __dirname
-                            }
-                        }
+                tailwindcss: {
+                    v4: {
+                        base: __dirname,
+                        cssEntries: path.resolve(__dirname, 'main.css')
                     }
                 }
             }
